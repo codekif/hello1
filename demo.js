b1a0707079dcb731d8c8a3c8c13a0ac719c1df95
codekif <pandiyarajacr7@gmail.com>
2024-03-16 15:44:53 +0530

    modifi

--- demo.js
+++ demo.js
@@ -17,3 +17,46 @@ function added(a,b)
 }
 var c=added(12,100)
 console.log(c)
+
+//If else
+var color="green"
+if(color == "red")
+{
+    console.log("Stop")
+}
+else if(color == "Yellow")
+{
+    console.log("Get ready")
+}
+else if(color == "green"){
+    console.log("Go")
+}
+
+// student mark 
+var score = 98
+if(score>=50 && score<80)
+{
+    console.log("You pass")
+}
+else if(score>=80 && score<=90)
+{
+    console.log("you got good mark")
+}
+else if(score<50)
+{
+    console.log("You fail")
+}
+else if(score>90)
+{
+    console.log("Excellent Work")
+}
+
+// for loop
+for(count=1;count<=10;count=count+1)
+{
+    console.log(count+"x2="+1*2 )
+}
+//'dom' document object model
+var p=document.getElementById("one")
+console.log(p.textContent="bye")
+p.textContent="bye World"
