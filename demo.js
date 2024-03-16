dff449c7484e4ea397b8670afe9c2f1cffd52976
codekif <pandiyarajacr7@gmail.com>
2024-03-16 14:08:25 +0530

    modifi

--- demo.js
+++ demo.js
@@ -1,1 +1,19 @@
+function add(a,b)
+{
+    console.log(a+b)
 
+}
+add(12,12)
+function area(l,b)
+{
+    var a=l+b
+    console.log(a)
+}
+area(11,15 )
+
+function added(a,b)
+{
+    return a+b
+}
+var c=added(12,100)
+console.log(c)
